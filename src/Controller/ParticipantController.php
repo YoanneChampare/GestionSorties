a37bfefad8a85d02d3916c243e18c09e46390202
Sfigaro <stephane.figaro2019@campus-eni.fr>
2020-04-15 14:44:53 +0200

    login+templates+nav

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -2,18 +2,34 @@
 
 namespace App\Controller;
 
+use App\Entity\Participant;
+use App\Form\LoginType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ParticipantController extends Controller
 {
     /**
-     * @Route("/participant", name="participant")
+     * @Route("/", name="participant")
      */
     public function index()
     {
         return $this->render('participant/index.html.twig', [
-            'controller_name' => 'ParticipantController',
+            'page_name'=>'Home',
+        ]);
+    }
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(){
+
+        $participant = new Participant();
+        $loginForm = $this->createForm(LoginType::class,$participant);
+
+        return $this->render('participant/login.html.twig', [
+            'loginForm'=>$loginForm->createView(),
+            'page_name'=>'Login',
         ]);
     }
 }
